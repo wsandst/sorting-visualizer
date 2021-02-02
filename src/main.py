4e9e8754a011ec4664b63f967e7491f3b9e5a5af
wsandst <williamsandstrom99@hotmail.com>
2021-02-03 00:10:30 +0100

    Add multiple new ways to display sorting

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from special_types import SList, ThreadManagment
 
 def main():
     lst = SList()
-    lst.shuffle_linear(1200)
+    lst.shuffle_linear(400)
     # Init all the sorting algorithms
     sorting_algos = []
 
