f0dc0c8392894127c06c6e8346086a54b0aba8b2
williamsandst <williamsandstrom99@hotmail.com>
2021-02-08 15:16:53 +0100

    Fix issues with sound

--- assets/sounds/generate_sound_files.py
+++ assets/sounds/generate_sound_files.py
@@ -5,7 +5,11 @@ import os
 count = 64
 print(f"Generating {count} different pitches")
 # 0.1 - 1.0
+"""for i in range(count):
+    pitch_tone = -8 + i*0.4
+    os.system(f"rubberband -t 0.1 -p {pitch_tone} tone.wav tone-{i}.wav") """
+
 for i in range(count):
-    pitch_tone = -32 + i*0.7
-    os.system(f"rubberband -t 0.2 -p {pitch_tone} harp.wav tone-{i}.wav")
+    pitch_tone = -16 + i*0.7
+    os.system(f"rubberband -t 0.5 -p {pitch_tone} harp.wav tone-{i}.wav")
 
